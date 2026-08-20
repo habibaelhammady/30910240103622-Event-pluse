348ab0bad74fe4a2f9b9fbfb838007101601fbce
habibaelhammady <habiba.elhammady@gmail.com>
2026-08-20 21:18:32 +0300

    fix: let the caller handle DB failures instead of process.exit(1)

--- config/db.js
+++ config/db.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () =>{
-     try{
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log('MongoDB is connected');
-     }catch (err){
-        console.log('Date base connection faild: ', err.message);
-        process.exit(1);
-     }
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log('MongoDB is connected');
 };
 
 module.exports = connectDB;
